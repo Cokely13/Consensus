3fffa03ffd9f5880eac5e95aa45b259833ff7734
Ryan Cokely <ryan.cokely@gmail.com>
2024-08-28 14:45:45 -0400

    models and associations done

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,45 @@
 //this is the access point for all things database related!
 
 const db = require('./db')
+const Group = require('./models/Group');
+const GroupMember = require('./models/GroupMember');
+const Question = require('./models/Question');
+const GroupQuestion = require('./models/GroupQuestion');
+const UserResponse = require('./models/UserResponse');
+const Consensus = require('./models/Consensus');
 
 const User = require('./models/User')
 
-//associations could go here!
+//associations:
+User.belongsToMany(Group, { through: GroupMember });
+Group.belongsToMany(User, { through: GroupMember });
+
+Group.belongsTo(User, { as: 'leader', foreignKey: 'leaderId' });
+
+Group.belongsToMany(Question, { through: GroupQuestion });
+Question.belongsToMany(Group, { through: GroupQuestion });
+
+Question.hasMany(UserResponse);
+UserResponse.belongsTo(Question);
+
+User.hasMany(UserResponse);
+UserResponse.belongsTo(User);
+
+Group.hasMany(Consensus);
+Consensus.belongsTo(Group);
+
+Question.hasMany(Consensus);
+Consensus.belongsTo(Question);
 
 module.exports = {
   db,
   models: {
     User,
+    Group,
+    GroupMember,
+    Question,
+    GroupQuestion,
+    UserResponse,
+    Consensus,
   },
 }
---
+++ server/db/models/Consensus.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Consensus = db.define('consensus', {
+  groupId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'groups',
+      key: 'id',
+    },
+  },
+  questionId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'questions',
+      key: 'id',
+    },
+  },
+  consensusAnswer: {
+    type: Sequelize.ENUM('option_a', 'option_b'),
+    allowNull: false,
+  },
+  calculatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = Consensus;
