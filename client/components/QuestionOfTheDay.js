1ad9e29b641c888cba8e4b4b915b107d4f37148c
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-03 16:27:28 -0400

    fixed voting

--- client/components/QuestionOfTheDay.js
+++ client/components/QuestionOfTheDay.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuestions } from '../store/allQuestionsStore';
+import { createUserResponse } from '../store/userResponsesStore'; // Import the createUserResponse action
 
 function QuestionOfTheDay() {
   const dispatch = useDispatch();
   const questions = useSelector((state) => state.allQuestions);
+  const { id } = useSelector(state => state.auth);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [hasVoted, setHasVoted] = useState(false);
 
@@ -21,8 +23,17 @@ function QuestionOfTheDay() {
   }, [questions]);
 
   const handleVote = (option) => {
-    // Here you would typically make an API call to record the user's vote
-    console.log(`User voted for: ${option}`);
+    // Create the userResponse object
+    const userResponse = {
+      userId: id, // Replace with actual logged-in user ID
+      questionId: selectedQuestion.id,
+      response: option,
+    };
+
+    // Dispatch the createUserResponse action to save the vote
+    dispatch(createUserResponse(userResponse));
+
+    // Set hasVoted to true to display the confirmation message
     setHasVoted(true);
   };
 
