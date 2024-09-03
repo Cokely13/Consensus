6d0fd7c8585533fc18c06fc13393539734dda105
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-03 17:39:36 -0400

    calculcating votes

--- client/components/Archive.js
+++ client/components/Archive.js
@@ -1,3 +1,44 @@
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchQuestions } from '../store/allQuestionsStore';
+
+// function Archive() {
+//   const dispatch = useDispatch();
+//   const questions = useSelector((state) => state.allQuestions);
+
+//   useEffect(() => {
+//     dispatch(fetchQuestions());
+//   }, [dispatch]);
+
+//   console.log("questions", questions)
+
+//   return (
+//     <div>
+//       <h2>Archive</h2>
+//       <div className="grid-container">
+//         <div className="grid-header">
+//           <div>Question</div>
+//           <div>Option A</div>
+//           <div>Votes for Option A</div>
+//           <div>Option B</div>
+//           <div>Votes for Option B</div>
+//         </div>
+//         {questions.map((question) => (
+//           <div key={question.id} className="grid-row">
+//             <div>{question.text}</div>
+//             <div>{question.optionA}</div>
+//             <div>{question.optionAVotes || 0}</div>
+//             <div>{question.optionB}</div>
+//             <div>{question.optionBVotes || 0}</div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Archive;
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuestions } from '../store/allQuestionsStore';
@@ -9,6 +50,10 @@ function Archive() {
   useEffect(() => {
     dispatch(fetchQuestions());
   }, [dispatch]);
+
+  const calculateVotes = (userResponses, option) => {
+    return userResponses.filter((response) => response.response === option).length;
+  };
 
   return (
     <div>
@@ -25,9 +70,9 @@ function Archive() {
           <div key={question.id} className="grid-row">
             <div>{question.text}</div>
             <div>{question.optionA}</div>
-            <div>{question.optionAVotes || 0}</div>
+            <div>{calculateVotes(question.user_responses, 'option_a')}</div>
             <div>{question.optionB}</div>
-            <div>{question.optionBVotes || 0}</div>
+            <div>{calculateVotes(question.user_responses, 'option_b')}</div>
           </div>
         ))}
       </div>
