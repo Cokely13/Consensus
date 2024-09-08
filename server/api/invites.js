f6a608341827c333594d8f36b056d4e864d282c1
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-07 21:46:04 -0400

    updated invites to add to groupmember

--- server/api/invites.js
+++ server/api/invites.js
@@ -1,53 +1,76 @@
-// // server/api/invites.js
 // const router = require('express').Router();
 // const {
 //   models: { Invite, User, Group },
 // } = require('../db');
 
-// module.exports = router
+// module.exports = router;
 
-
+// // GET: Get all invites with details of users and groups
 // router.get('/', async (req, res, next) => {
 //   try {
-//     const invites= await Invite.findAll( {include : [User, Group]}
-//     );
+//     const invites = await Invite.findAll({
+//       include: [
+//         { model: User, as: 'inviter', attributes: ['id', 'username'] }, // Inviter details
+//         { model: User, as: 'invitee', attributes: ['id', 'username'] }, // Invitee details
+//         { model: Group, attributes: ['id', 'name'] }, // Group details
+//       ],
+//     });
 //     res.json(invites);
 //   } catch (err) {
 //     next(err);
 //   }
 // });
 
-// //POST: add a new Invite
-// router.post("/", async (req, res, next) => {
+// // POST: Add a new Invite
+// router.post('/', async (req, res, next) => {
 //   try {
-//     res.status(201).send(await Invite.create(req.body));
+//     const newInvite = await Invite.create(req.body);
+//     res.status(201).send(newInvite);
 //   } catch (error) {
 //     next(error);
 //   }
 // });
 
+// // PUT: Update an Invite status (accept/reject)
 // router.put('/:id', async (req, res, next) => {
 //   try {
-//     const invite= await Invite.findByPk(req.params.id)
-//     res.send(await invite.update(req.body));
+//     const invite = await Invite.findByPk(req.params.id);
+//     if (!invite) {
+//       return res.status(404).send('Invite not found');
+//     }
+//     const updatedInvite = await invite.update(req.body);
+//     res.send(updatedInvite);
 //   } catch (error) {
 //     next(error);
 //   }
 // });
 
-// //Get read all invites
+// // GET: Get a specific Invite by ID
 // router.get('/:id', async (req, res, next) => {
 //   try {
-//     const invite = await Invite.findByPk(req.params.id,{include : [User, Invite]});
+//     const invite = await Invite.findByPk(req.params.id, {
+//       include: [
+//         { model: User, as: 'inviter', attributes: ['id', 'username'] },
+//         { model: User, as: 'invitee', attributes: ['id', 'username'] },
+//         { model: Group, attributes: ['id', 'name'] },
+//       ],
+//     });
+//     if (!invite) {
+//       return res.status(404).send('Invite not found');
+//     }
 //     res.json(invite);
 //   } catch (err) {
 //     next(err);
 //   }
 // });
 
+// // DELETE: Delete an Invite
 // router.delete('/:id', async (req, res, next) => {
 //   try {
 //     const invite = await Invite.findByPk(req.params.id);
+//     if (!invite) {
+//       return res.status(404).send('Invite not found');
+//     }
 //     await invite.destroy();
 //     res.send(invite);
 //   } catch (error) {
@@ -57,7 +80,7 @@
 
 const router = require('express').Router();
 const {
-  models: { Invite, User, Group },
+  models: { Invite, User, Group, GroupMember },
 } = require('../db');
 
 module.exports = router;
@@ -95,7 +118,24 @@ router.put('/:id', async (req, res, next) => {
     if (!invite) {
       return res.status(404).send('Invite not found');
     }
+
+    // Update the invite status
     const updatedInvite = await invite.update(req.body);
+
+    // If the invite is accepted, add the user to the group as a member
+    if (updatedInvite.status === 'accepted') {
+      const groupId = updatedInvite.groupId;
+      const userId = updatedInvite.inviteeId;
+
+      // Check if the user is already a member of the group
+      const existingMember = await GroupMember.findOne({ where: { groupId, userId } });
+
+      // Add the user to the group if they are not already a member
+      if (!existingMember) {
+        await GroupMember.create({ groupId, userId });
+      }
+    }
+
     res.send(updatedInvite);
   } catch (error) {
     next(error);
