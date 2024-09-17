28dc35b44bc4d84ff4757bbd28a791e95f739728
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-17 17:07:01 -0400

    added image to users

--- client/components/Users.js
+++ client/components/Users.js
@@ -1,3 +1,37 @@
+// import { useDispatch, useSelector } from 'react-redux';
+// import React, { useEffect } from 'react';
+// import { fetchUsers } from '../store/allUsersStore';
+// import { Link } from 'react-router-dom';
+
+// function UserPage() {
+//   const dispatch = useDispatch();
+//   const users = useSelector((state) => state.allUsers);
+//   const { id: currentUserId } = useSelector((state) => state.auth);
+
+//   useEffect(() => {
+//     dispatch(fetchUsers());
+//   }, [dispatch]);
+
+//   const filteredUsers = users.filter((user) => user.id !== currentUserId);
+
+//   return (
+//     <div className="page-container">
+//       <h1 className="page-heading"><u><b>Users</b></u></h1>
+//       <div className="card-container">
+//         {filteredUsers.map((user) => (
+//           <div key={user.id} className="card">
+//             <Link to={`/users/${user.id}`} className="card-link">
+//               <h2 className="card-heading">{user.username}</h2>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default UserPage;
+
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { fetchUsers } from '../store/allUsersStore';
@@ -16,11 +50,24 @@ function UserPage() {
 
   return (
     <div className="page-container">
-      <h1 className="page-heading"><u><b>Users</b></u></h1>
+      <h1 className="page-heading">
+        <u>
+          <b>Users</b>
+        </u>
+      </h1>
       <div className="card-container">
         {filteredUsers.map((user) => (
           <div key={user.id} className="card">
             <Link to={`/users/${user.id}`} className="card-link">
+              {/* Display User Image */}
+              {user.image && (
+                <img
+                  src={user.image}
+                  alt={user.username}
+                  className="user-image-thumbnail"
+                />
+              )}
+              {/* Display User Name */}
               <h2 className="card-heading">{user.username}</h2>
             </Link>
           </div>
