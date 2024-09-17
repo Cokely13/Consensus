232f3f9f9de8b7d409baa3927bb0867a26052c42
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-17 17:01:34 -0400

    added image to mygroups

--- client/components/Archive.js
+++ client/components/Archive.js
@@ -1,111 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchQuestions } from '../store/allQuestionsStore';
-// import { createConsensus } from '../store/allConsensusesStore'; // Import createConsensus action
-// import { updateSingleQuestion } from '../store/singleQuestionStore'; // Import updateQuestion action
-
-// function Archive() {
-//   const dispatch = useDispatch();
-//   const questions = useSelector((state) => state.allQuestions);
-//   const user = useSelector((state) => state.auth); // Get user information from the Redux store
-
-//   useEffect(() => {
-//     dispatch(fetchQuestions());
-//   }, [dispatch]);
-
-//   const calculateVotes = (userResponses, option) => {
-//     return userResponses.filter((response) => response.response === option).length;
-//   };
-
-//   const handleCreateConsensus = async (question) => {
-//     const optionAVotes = calculateVotes(question.user_responses, 'option_a');
-//     const optionBVotes = calculateVotes(question.user_responses, 'option_b');
-
-//     if (optionAVotes === optionBVotes) {
-//       alert('Cannot Do, No Consensus');
-//       return;
-//     }
-
-//     // Determine the consensus answer
-//     const consensusAnswer = optionAVotes > optionBVotes ? 'option_a' : 'option_b';
-
-//     // Create a new consensus
-//     await dispatch(
-//       createConsensus({
-//         questionId: question.id,
-//         consensusAnswer,
-//         calculatedAt: new Date().toISOString(),
-//       })
-//     );
-
-//     // Refetch the questions to refresh the component state
-//     dispatch(fetchQuestions());
-//   };
-
-//   const handleReopenQuestion = async (question) => {
-//     // Reopen the question by setting expired to false
-//     await dispatch(updateSingleQuestion(question.id, { expired: false }));
-
-//     // Refetch the questions to refresh the component state
-//     dispatch(fetchQuestions());
-//   };
-
-//   // Check if the user is an admin
-//   if (!user || !user.admin) {
-//     return <p>You Are not an Admin!</p>;
-//   }
-
-//   return (
-//     <div>
-//       <h2>Archive</h2>
-//       <div className="grid-container">
-//         <div className="grid-header">
-//           <div>Question</div>
-//           <div>Option A</div>
-//           <div>Votes for Option A</div>
-//           <div>Option B</div>
-//           <div>Votes for Option B</div>
-//           <div>Date Asked</div>
-//           <div>Action</div>
-//         </div>
-//         {questions.map((question) => {
-//           const optionAVotes = calculateVotes(question.user_responses, 'option_a');
-//           const optionBVotes = calculateVotes(question.user_responses, 'option_b');
-
-//           const highlightOptionA = optionAVotes > optionBVotes ? 'highlight' : '';
-//           const highlightOptionB = optionBVotes > optionAVotes ? 'highlight' : '';
-
-//           const isExpired = question.consensus.length ? true : false;
-
-//           return (
-//             <div key={question.id} className="grid-row">
-//               <div>{question.text}</div>
-//               <div className={highlightOptionA}>{question.optionA}</div>
-//               <div>{optionAVotes}</div>
-//               <div className={highlightOptionB}>{question.optionB}</div>
-//               <div>{optionBVotes}</div>
-//               <div>{question.dateAsked}</div>
-//               <div>
-//                 {isExpired ? (
-//                   <button onClick={() => handleReopenQuestion(question)}>
-//                     Reopen the Question
-//                   </button>
-//                 ) : (
-//                   <button onClick={() => handleCreateConsensus(question)}>
-//                     Create Consensus
-//                   </button>
-//                 )}
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Archive;
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuestions } from '../store/allQuestionsStore';
@@ -180,14 +72,14 @@ function Archive() {
           const isExpired = question.consensus.length ? true : false;
 
           return (
-            <>
-              <div key={`${question.id}-text`} className="archive-grid-cell">{question.text}</div>
-              <div key={`${question.id}-optionA`} className={`archive-grid-cell ${highlightOptionA}`}>{question.optionA}</div>
-              <div key={`${question.id}-votesA`} className="archive-grid-cell">{optionAVotes}</div>
-              <div key={`${question.id}-optionB`} className={`archive-grid-cell ${highlightOptionB}`}>{question.optionB}</div>
-              <div key={`${question.id}-votesB`} className="archive-grid-cell">{optionBVotes}</div>
-              <div key={`${question.id}-date`} className="archive-grid-cell">{new Date(question.dateAsked).toLocaleDateString()}</div>
-              <div key={`${question.id}-action`} className="archive-grid-cell">
+            <React.Fragment key={question.id}>
+              <div className="archive-grid-cell">{question.text}</div>
+              <div className={`archive-grid-cell ${highlightOptionA}`}>{question.optionA}</div>
+              <div className="archive-grid-cell">{optionAVotes}</div>
+              <div className={`archive-grid-cell ${highlightOptionB}`}>{question.optionB}</div>
+              <div className="archive-grid-cell">{optionBVotes}</div>
+              <div className="archive-grid-cell">{new Date(question.dateAsked).toLocaleDateString()}</div>
+              <div className="archive-grid-cell">
                 {isExpired ? (
                   <button
                     className="archive-button"
@@ -204,7 +96,7 @@ function Archive() {
                   </button>
                 )}
               </div>
-            </>
+            </React.Fragment>
           );
         })}
       </div>
--- client/components/MyGroups.js
+++ client/components/MyGroups.js
@@ -1,3 +1,62 @@
+// import { useDispatch, useSelector } from 'react-redux';
+// import React, { useEffect } from 'react';
+// import { fetchGroups } from '../store/allGroupsStore';
+// import { fetchInvites } from '../store/allInvitesStore';
+// import { Link } from 'react-router-dom';
+
+// function MyGroups() {
+//   const dispatch = useDispatch();
+//   const groups = useSelector((state) => state.allGroups);
+//   const invites = useSelector((state) => state.allInvites);
+//   const { id: currentUserId } = useSelector((state) => state.auth);
+
+//   useEffect(() => {
+//     dispatch(fetchGroups());
+//     dispatch(fetchInvites());
+//   }, [dispatch]);
+
+//   // Filter groups to show only those where the current user is a member
+//   const filteredGroups = groups.filter(
+//     (group) =>
+//       group.group_members && group.group_members.some((member) => member.userId === currentUserId)
+//   );
+
+//   return (
+//     <div className="page-container">
+//       <h1 className="page-heading">
+//         <u>
+//           <b>My Groups</b>
+//         </u>
+//       </h1>
+//       <div className="card-container">
+//         {filteredGroups.map((group) => {
+//           // Check if there is a pending invite for the user to this group
+//           const hasPendingInvite = invites.some(
+//             (invite) =>
+//               invite.groupId === group.id &&
+//               invite.inviteeId === currentUserId &&
+//               invite.status === 'pending'
+//           );
+
+//           return (
+//             <div key={group.id} className="card">
+//               <Link to={`/groups/${group.id}`} className="card-link">
+//                 <h2 className="card-heading">
+//                   {group.name} - # of Members {group.group_members ? group.group_members.length : 0}
+//                   <span> (Member)</span>
+//                   {hasPendingInvite && <span> (Invite)</span>}
+//                 </h2>
+//               </Link>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MyGroups;
+
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { fetchGroups } from '../store/allGroupsStore';
@@ -41,11 +100,22 @@ function MyGroups() {
           return (
             <div key={group.id} className="card">
               <Link to={`/groups/${group.id}`} className="card-link">
-                <h2 className="card-heading">
-                  {group.name} - # of Members {group.group_members ? group.group_members.length : 0}
-                  <span> (Member)</span>
-                  {hasPendingInvite && <span> (Invite)</span>}
-                </h2>
+                <div className="group-info">
+                  {/* Group Image */}
+                  {group.image && (
+                    <img
+                      src={group.image}
+                      alt={`${group.name} Group`}
+                      className="group-image-thumbnail"
+                    />
+                  )}
+                  {/* Group Name and Info */}
+                  <h2 className="card-heading">
+                    {group.name} - # of Members {group.group_members ? group.group_members.length : 0}
+                    <span> (Member)</span>
+                    {hasPendingInvite && <span> (Invite)</span>}
+                  </h2>
+                </div>
               </Link>
             </div>
           );
