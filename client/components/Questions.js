82790696b87d9d902d49f2cb289cf94101c5c68f
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-17 16:20:38 -0400

    updated questions

--- client/components/Questions.js
+++ client/components/Questions.js
@@ -1,53 +1,3 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import React, { useEffect } from 'react';
-// import { fetchQuestions } from '../store/allQuestionsStore';
-
-// function Questions() {
-//   const dispatch = useDispatch();
-//   const questions = useSelector((state) => state.allQuestions);
-
-//   useEffect(() => {
-//     dispatch(fetchQuestions());
-//   }, [dispatch]);
-
-//   // Sort questions by dateAsked in descending order (newest first)
-//   const sortedQuestions = [...questions].sort(
-//     (a, b) => new Date(b.dateAsked) - new Date(a.dateAsked)
-//   );
-
-//   console.log("questions", questions);
-
-//   return (
-//     <div className="page-container">
-//       <h2 className="page-heading">Questions</h2>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Question</th>
-//             <th>Option A</th>
-//             <th>Option B</th>
-//             <th>Date Asked</th>
-//             <th>Status</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {sortedQuestions.map((question) => (
-//             <tr key={question.id}>
-//               <td>{question.text}</td>
-//               <td>{question.optionA}</td>
-//               <td>{question.optionB}</td>
-//               <td>{new Date(question.dateAsked).toLocaleDateString()}</td>
-//               <td>{question.status}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default Questions;
-
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { fetchQuestions } from '../store/allQuestionsStore';
@@ -77,10 +27,12 @@ function Questions() {
           <tr>
             <th>Question</th>
             <th>Option A</th>
+            <th>Image A</th>
             <th>Option B</th>
+            <th>Image B</th>
             <th>Date Asked</th>
             <th>Status</th>
-            {user.admin && <th>Action</th>} {/* Display Action column for admins */}
+            {user.admin && <th>Action</th>}
           </tr>
         </thead>
         <tbody>
@@ -88,10 +40,26 @@ function Questions() {
             <tr key={question.id}>
               <td>{question.text}</td>
               <td>{question.optionA}</td>
+              <td>
+                {question.imageA && (
+                  <img
+                    src={question.imageA}
+                    style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+                  />
+                )}
+              </td>
               <td>{question.optionB}</td>
+              <td>
+                {question.imageB && (
+                  <img
+                    src={question.imageB}
+                    style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+                  />
+                )}
+              </td>
               <td>{new Date(question.dateAsked).toLocaleDateString()}</td>
               <td>{question.status}</td>
-              {user.admin && ( // Show Edit button for admins
+              {user.admin && (
                 <td>
                   <Link to={`/edit-question/${question.id}`}>
                     <button>Edit Question</button>
