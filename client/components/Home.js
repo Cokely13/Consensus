a43c688d58d8c3bee92907675c20654f7ddbc62c
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-18 11:51:00 -0400

    fixed homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,16 +1,46 @@
+// import React from 'react'
+// import {connect} from 'react-redux'
+// import QuestionOfTheDay from './QuestionOfTheDay'
+
+// /**
+//  * COMPONENT
+//  */
+// export const Home = props => {
+//   const {username} = props
+
+//   return (
+//     <div>
+//        <h1>Consensus</h1>
+//       <QuestionOfTheDay/>
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     username: state.auth.username
+//   }
+// }
+
+// export default connect(mapState)(Home)
+
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import QuestionOfTheDay from './QuestionOfTheDay'
 
 /**
  * COMPONENT
  */
 export const Home = props => {
-  const {username} = props
+  const { username } = props
 
   return (
-    <div>
-      <QuestionOfTheDay/>
+    <div className="home-container">
+      <h1 className="home-heading">Consensus</h1>
+      <QuestionOfTheDay />
     </div>
   )
 }
