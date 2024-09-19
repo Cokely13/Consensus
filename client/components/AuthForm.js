4ef5334db515865a2f51c66e7313484e3a75dadc
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-19 12:05:31 -0400

    log in screen css

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,71 +1,141 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+// import React from 'react'
+// import {connect} from 'react-redux'
+// import {authenticate} from '../store'
+
+// /**
+//  * COMPONENT
+//  */
+// const AuthForm = props => {
+//   const {name, displayName, handleSubmit, error} = props
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit} name={name}>
+//         <div>
+//           <label htmlFor="username">
+//             <small>Username</small>
+//           </label>
+//           <input name="username" type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="password">
+//             <small>Password</small>
+//           </label>
+//           <input name="password" type="password" />
+//         </div>
+//         <div>
+//           <button type="submit">{displayName}</button>
+//         </div>
+//         {error && error.response && <div> {error.response.data} </div>}
+//       </form>
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  *   Note that we have two different sets of 'mapStateToProps' functions -
+//  *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+//  *   function, and share the same Component. This is a good example of how we
+//  *   can stay DRY with interfaces that are very similar to each other!
+//  */
+// const mapLogin = state => {
+//   return {
+//     name: 'login',
+//     displayName: 'Login',
+//     error: state.auth.error
+//   }
+// }
+
+// const mapSignup = state => {
+//   return {
+//     name: 'signup',
+//     displayName: 'Sign Up',
+//     error: state.auth.error
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     handleSubmit(evt) {
+//       evt.preventDefault()
+//       const formName = evt.target.name
+//       const username = evt.target.username.value
+//       const password = evt.target.password.value
+//       dispatch(authenticate(username, password, formName))
+//     }
+//   }
+// }
+
+// export const Login = connect(mapLogin, mapDispatch)(AuthForm)
+// export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from '../store';
 
 /**
  * COMPONENT
  */
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error} = props
+const AuthForm = (props) => {
+  const { name, displayName, handleSubmit, error } = props;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
+    <div className="auth-container">
+      <form onSubmit={handleSubmit} name={name} className="auth-form">
+        <div className="form-group">
+          <label htmlFor="username" className="form-label">
+            Username
           </label>
-          <input name="username" type="text" />
+          <input name="username" type="text" className="form-input" required />
         </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">
+            Password
           </label>
-          <input name="password" type="password" />
+          <input name="password" type="password" className="form-input" required />
         </div>
-        <div>
-          <button type="submit">{displayName}</button>
+        <div className="form-group">
+          <button type="submit" className="auth-button">
+            {displayName}
+          </button>
         </div>
-        {error && error.response && <div> {error.response.data} </div>}
+        {error && error.response && <div className="error-message"> {error.response.data} </div>}
       </form>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = state => {
+const mapLogin = (state) => {
   return {
     name: 'login',
     displayName: 'Login',
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+  };
+};
 
-const mapSignup = state => {
+const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const username = evt.target.username.value
-      const password = evt.target.password.value
-      dispatch(authenticate(username, password, formName))
-    }
-  }
-}
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const password = evt.target.password.value;
+      dispatch(authenticate(username, password, formName));
+    },
+  };
+};
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Login = connect(mapLogin, mapDispatch)(AuthForm);
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,32 +1,3 @@
-// import React from 'react'
-// import {connect} from 'react-redux'
-// import QuestionOfTheDay from './QuestionOfTheDay'
-
-// /**
-//  * COMPONENT
-//  */
-// export const Home = props => {
-//   const {username} = props
-
-//   return (
-//     <div>
-//        <h1>Consensus</h1>
-//       <QuestionOfTheDay/>
-//     </div>
-//   )
-// }
-
-// /**
-//  * CONTAINER
-//  */
-// const mapState = state => {
-//   return {
-//     username: state.auth.username
-//   }
-// }
-
-// export default connect(mapState)(Home)
-
 import React from 'react'
 import { connect } from 'react-redux'
 import QuestionOfTheDay from './QuestionOfTheDay'
