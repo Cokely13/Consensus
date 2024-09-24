c24d78db614fc11ff29ec3ac98b29f2e6694c6ca
Ryan Cokely <ryan.cokely@gmail.com>
2024-09-24 18:24:18 -0400

    groups css

--- client/components/Groups.js
+++ client/components/Groups.js
@@ -1,5 +1,60 @@
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
+// import React, { useEffect } from 'react';
+// import { fetchGroups } from '../store/allGroupsStore';
+// import { fetchInvites } from '../store/allInvitesStore';
+// import { Link } from 'react-router-dom';
+
+// function Groups() {
+//   const dispatch = useDispatch();
+//   const groups = useSelector((state) => state.allGroups);
+//   const invites = useSelector((state) => state.allInvites);
+//   const { id: currentUserId } = useSelector((state) => state.auth);
+
+//   useEffect(() => {
+//     dispatch(fetchGroups());
+//     dispatch(fetchInvites());
+//   }, [dispatch]);
+
+//   const filteredGroups = groups.filter((group) => group.id !== currentUserId);
+
+//   return (
+//     <div className="page-container">
+//       <h1 className="page-heading"><u><b>Groups</b></u></h1>
+//       <div className="card-container">
+//         {groups.map((group) => {
+//           const isMember = group.group_members && group.group_members.some(member => member.userId == currentUserId);
+//           const hasPendingInvite = invites.some(invite => invite.groupId === group.id && invite.inviteeId == currentUserId && invite.status === 'pending');
+
+//           return (
+//             <div key={group.id} className="card">
+//               <Link to={`/groups/${group.id}`} className="card-link">
+//                 <div className="card-image-container">
+//                   {group.image && (
+//                     <img
+//                       src={group.image}
+//                       alt={`${group.name} Group`}
+//                       className="card-image"
+//                     />
+//                   )}
+//                 </div>
+//                 <h2 className="card-heading">
+//                   {group.name} - # of Members {group.group_members ? group.group_members.length : 0}
+//                   {isMember && <span> (Member)</span>}
+//                   {hasPendingInvite && <span> (Invite)</span>}
+//                 </h2>
+//               </Link>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Groups;
+
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchGroups } from '../store/allGroupsStore';
 import { fetchInvites } from '../store/allInvitesStore';
 import { Link } from 'react-router-dom';
@@ -15,33 +70,52 @@ function Groups() {
     dispatch(fetchInvites());
   }, [dispatch]);
 
-  const filteredGroups = groups.filter((group) => group.id !== currentUserId);
-
   return (
-    <div className="page-container">
-      <h1 className="page-heading"><u><b>Groups</b></u></h1>
-      <div className="card-container">
+    <div className="groups-page-container">
+      <h1 className="groups-page-heading">
+        <u>
+          <b>Groups</b>
+        </u>
+      </h1>
+      <div className="groups-page-card-container">
         {groups.map((group) => {
-          const isMember = group.group_members && group.group_members.some(member => member.userId == currentUserId);
-          const hasPendingInvite = invites.some(invite => invite.groupId === group.id && invite.inviteeId == currentUserId && invite.status === 'pending');
+          const isMember =
+            group.group_members &&
+            group.group_members.some((member) => member.userId === currentUserId);
+          const hasPendingInvite = invites.some(
+            (invite) =>
+              invite.groupId === group.id &&
+              invite.inviteeId === currentUserId &&
+              invite.status === 'pending'
+          );
 
           return (
-            <div key={group.id} className="card">
-              <Link to={`/groups/${group.id}`} className="card-link">
-                <div className="card-image-container">
-                  {group.image && (
+            <div key={group.id} className="groups-page-card">
+              <Link to={`/groups/${group.id}`} className="groups-page-card-link">
+                <div className="groups-page-card-image-container">
+                  {group.image ? (
                     <img
                       src={group.image}
                       alt={`${group.name} Group`}
-                      className="card-image"
+                      className="groups-page-card-image"
                     />
+                  ) : (
+                    <div className="groups-page-card-placeholder">
+                      <i className="fas fa-users"></i>
+                    </div>
                   )}
-                </div>
-                <h2 className="card-heading">
-                  {group.name} - # of Members {group.group_members ? group.group_members.length : 0}
-                  {isMember && <span> (Member)</span>}
-                  {hasPendingInvite && <span> (Invite)</span>}
-                </h2>
+                  </div>
+                  {isMember && (
+                    <span className="groups-page-card-status member">Member</span>
+                  )}
+                  {hasPendingInvite && (
+                    <span className="groups-page-card-status pending">Invite Pending</span>
+                  )}
+
+                <h2 className="groups-page-card-heading">{group.name}</h2>
+                <p className="groups-page-card-members">
+                  Members: {group.group_members ? group.group_members.length : 0}
+                </p>
               </Link>
             </div>
           );
--- client/components/Users.js
+++ client/components/Users.js
@@ -16,11 +16,24 @@
 
 //   return (
 //     <div className="page-container">
-//       <h1 className="page-heading"><u><b>Users</b></u></h1>
+//       <h1 className="page-heading">
+//         <u>
+//           <b>Users</b>
+//         </u>
+//       </h1>
 //       <div className="card-container">
 //         {filteredUsers.map((user) => (
 //           <div key={user.id} className="card">
 //             <Link to={`/users/${user.id}`} className="card-link">
+//               {/* Display User Image */}
+//               {user.image && (
+//                 <img
+//                   src={user.image}
+//                   alt={user.username}
+//                   className="user-image-thumbnail"
+//                 />
+//               )}
+//               {/* Display User Name */}
 //               <h2 className="card-heading">{user.username}</h2>
 //             </Link>
 //           </div>
@@ -32,8 +45,8 @@
 
 // export default UserPage;
 
-import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store/allUsersStore';
 import { Link } from 'react-router-dom';
 
@@ -49,26 +62,30 @@ function UserPage() {
   const filteredUsers = users.filter((user) => user.id !== currentUserId);
 
   return (
-    <div className="page-container">
-      <h1 className="page-heading">
+    <div className="user-page-container">
+      <h1 className="user-page-heading">
         <u>
           <b>Users</b>
         </u>
       </h1>
-      <div className="card-container">
+      <div className="user-page-card-container">
         {filteredUsers.map((user) => (
-          <div key={user.id} className="card">
-            <Link to={`/users/${user.id}`} className="card-link">
-              {/* Display User Image */}
-              {user.image && (
-                <img
-                  src={user.image}
-                  alt={user.username}
-                  className="user-image-thumbnail"
-                />
-              )}
-              {/* Display User Name */}
-              <h2 className="card-heading">{user.username}</h2>
+          <div key={user.id} className="user-page-card">
+            <Link to={`/users/${user.id}`} className="user-page-card-link">
+              <div className="user-page-card-image-container">
+                {user.image ? (
+                  <img
+                    src={user.image}
+                    alt={user.username}
+                    className="user-page-card-image"
+                  />
+                ) : (
+                  <div className="user-page-card-placeholder">
+                    <i className="fas fa-user-circle"></i>
+                  </div>
+                )}
+              </div>
+              <h2 className="user-page-card-username">{user.username}</h2>
             </Link>
           </div>
         ))}
